b44ca9bd7d2796a6daedb8c2dc59e195e9c38aeb
Karna <57597042+Karna1014@users.noreply.github.com>
2020-05-06 19:01:33 -0400

    file not needed

---
+++ src/components/index.js
@@ -0,0 +1,41 @@
+// import React, { Component } from "react";
+// import data from "./emp.json";
+
+// // import "./style.css";
+
+// class Table extends Component {
+//   // Setting the component's initial state
+
+//   render() {
+//     return (
+//       <div className="container">
+//         <table className="table table-hover table-striped">
+//           <thead>
+//             <tr>
+//               <th>Id</th>
+//               <th>First Name</th>
+//               <th>Last Name</th>
+//               <th>Email Address</th>
+//               <th>Department</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {
+//               data.data.map((emp) => (
+//                 <tr>
+//                   <td>{emp[0]}</td>
+//                   <td>{emp[1]}</td>
+//                   <td>{emp[2]}</td>
+//                   <td>{emp[3]}</td>
+//                   <td>{emp[4]}</td>
+//                 </tr>
+//               ))
+//             }
+//           </tbody>
+//         </table>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Table
