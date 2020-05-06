a38906d104b1de4fb27eddb82da25f1d621cd78f
Karna <57597042+Karna1014@users.noreply.github.com>
2020-05-06 19:01:15 -0400

    added table code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BootstrapTable from 'react-bootstrap-table-next';
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
+import data from "./components/emp.json";
+import "react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter";
+import "react-bootstrap-table-next/dist/react-bootstrap-table-next";
+
+
+const employees = data.data;
+
+const columns = [
+  {
+      "dataField": "ID",
+      "text": "Employee ID",
+      "sort": true
+  },
+  {
+      "dataField": "First Name",
+      "text": "First Name",
+      "sort": true,
+      "filter": textFilter({
+        placeholder: 'Search by..'
+      })
+  },
+  {
+      "dataField": "Last Name",
+      "text": "Last Name",
+      "sort": true
+  },
+  {
+      "dataField": "Email",
+      "text": "Email",
+      "sort": true
+  },
+  {
+      "dataField": "Department",
+      "text": "Department",
+      "sort": true
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BootstrapTable keyField="ID" data={employees} columns={columns} filter={ filterFactory() }></BootstrapTable>
     </div>
   );
 }
